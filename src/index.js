1e8f738935a716d6ab8775c1b6fbf7c4873ab661
DoraH <poyuhq@gmail.com>
2022-08-01 10:51:22 +0800

    更新 HashRouter

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { UserTokenProvider, UserNicknameProvider } from './contexts/UserContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserTokenProvider>
         <UserNicknameProvider>
           <App />
         </UserNicknameProvider>
       </UserTokenProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
